c4889ef4fb86756550bead92e5193ca3aec9571e
Matthew Marriott <72930889+matthewmarriott@users.noreply.github.com>
2025-05-04 12:30:32 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Fieldwork Fitness</h1>
-      <p>This is the homepage for your expedition training program.</p>
-      <nav>
-        <Link href="/about">About</Link> | 
-        <Link href="/contact">Contact</Link>
-      </nav>
+      <p>This is your expedition training planner.</p>
     </div>
   );
 }
